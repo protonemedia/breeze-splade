653a89be3922505b7075571e8c8daf64c27a1db0
Pascal Baljet <pascal@pascalbaljet.nl>
2022-09-02 15:08:08 +0200

    Fixed dropdowns and links

--- stubs/splade/resources/views/components/dropdown.blade.php
+++ stubs/splade/resources/views/components/dropdown.blade.php
@@ -1,19 +1,6 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white'])
+@props(['width' => '48'])
 
 @php
-switch ($align) {
-    case 'left':
-        $alignmentClasses = 'origin-top-left left-0';
-        break;
-    case 'top':
-        $alignmentClasses = 'origin-top';
-        break;
-    case 'right':
-    default:
-        $alignmentClasses = 'origin-top-right right-0';
-        break;
-}
-
 switch ($width) {
     case '48':
         $width = 'w-48';
@@ -21,26 +8,12 @@ switch ($width) {
 }
 @endphp
 
-<x-splade-toggle>
-    <div class="relative">
-        <div @click="toggle">
-            {{ $trigger }}
-        </div>
+<x-splade-dropdown {{ $attributes->except('width') }}>
+    <x-slot:trigger>
+        {{ $trigger }}
+    </x-slot:trigger>
 
-        <transition
-            enter-active-class="transition ease-out duration-200"
-            enter-from-class="transform opacity-0 scale-95"
-            enter-to-class="transform opacity-100 scale-100"
-            leave-active-class="transition ease-in duration-75"
-            leave-from-class="transform opacity-100 scale-100"
-            leave-to-class="transform opacity-0 scale-95">
-            <div v-show="toggled"
-                    class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
-                    @click="setToggle(false)">
-                <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
-                    {{ $content }}
-                </div>
-            </div>
-        </transition>
+    <div class="mt-2 {{ $width }} rounded-md shadow-lg ring-1 ring-black ring-opacity-5 py-1 bg-white">
+        {{ $content }}
     </div>
-</x-splade-toggle>
+</x-splade-dropdown>
