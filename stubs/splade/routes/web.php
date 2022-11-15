ede69e04749693b95afa90d52497fb87f171ab45
Pascal Baljet <pascal@pascalbaljet.nl>
2022-11-15 08:24:22 +0100

    Splade v0.7

--- stubs/splade/routes/web.php
+++ stubs/splade/routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('splade')->group(function () {
+    // Registers routes to support Table Bulk Actions and Exports...
     Route::spladeTable();
 
     Route::get('/', function () {
@@ -26,5 +27,5 @@ Route::middleware('splade')->group(function () {
         })->name('dashboard');
     });
 
-    require __DIR__.'/auth.php';
+    require __DIR__ . '/auth.php';
 });
