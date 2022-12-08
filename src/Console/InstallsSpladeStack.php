6a556b40d869854c2437aa88340dfd891e1b299e
pascalbaljet <pascalbaljet@users.noreply.github.com>
2022-12-08 11:59:11 +0000

    Fix styling

--- src/Console/InstallsSpladeStack.php
+++ src/Console/InstallsSpladeStack.php
@@ -26,36 +26,36 @@ trait InstallsSpladeStack
         $this->updateNodePackages(function ($packages) {
             return [
                 '@protonemedia/laravel-splade' => '^1.1.0',
-                '@tailwindcss/forms'           => '^0.5.3',
-                '@tailwindcss/typography'      => '^0.5.2',
-                '@vitejs/plugin-vue'           => '^3.0.0',
-                'autoprefixer'                 => '^10.4.12',
-                'laravel-vite-plugin'          => '^0.5.0',
-                'postcss'                      => '^8.4.18',
-                'tailwindcss'                  => '^3.2.1',
-                'vite'                         => '^3.0.0',
-                'vue'                          => '^3.2.41',
+                '@tailwindcss/forms' => '^0.5.3',
+                '@tailwindcss/typography' => '^0.5.2',
+                '@vitejs/plugin-vue' => '^3.0.0',
+                'autoprefixer' => '^10.4.12',
+                'laravel-vite-plugin' => '^0.5.0',
+                'postcss' => '^8.4.18',
+                'tailwindcss' => '^3.2.1',
+                'vite' => '^3.0.0',
+                'vue' => '^3.2.41',
             ] + $packages;
         });
 
         // Add SSR build step...
         $this->updateNodeScript();
 
-        $defaultStubsDir      = __DIR__ . '/../../stubs/default/';
-        $spladeBreezeStubsDir = __DIR__ . '/../../stubs/splade/';
-        $spladeBaseStubsDir   = base_path('vendor/protonemedia/laravel-splade/stubs/');
+        $defaultStubsDir = __DIR__.'/../../stubs/default/';
+        $spladeBreezeStubsDir = __DIR__.'/../../stubs/splade/';
+        $spladeBaseStubsDir = base_path('vendor/protonemedia/laravel-splade/stubs/');
 
         // Controllers...
         (new Filesystem)->ensureDirectoryExists(app_path('Http/Controllers'));
-        (new Filesystem)->copyDirectory($defaultStubsDir . 'App/Http/Controllers', app_path('Http/Controllers'));
+        (new Filesystem)->copyDirectory($defaultStubsDir.'App/Http/Controllers', app_path('Http/Controllers'));
 
         // Requests...
         (new Filesystem)->ensureDirectoryExists(app_path('Http/Requests'));
-        (new Filesystem)->copyDirectory($defaultStubsDir . 'App/Http/Requests', app_path('Http/Requests'));
+        (new Filesystem)->copyDirectory($defaultStubsDir.'App/Http/Requests', app_path('Http/Requests'));
 
         // Views...
         (new Filesystem)->ensureDirectoryExists(resource_path('views'));
-        (new Filesystem)->copyDirectory($spladeBreezeStubsDir . 'resources/views', resource_path('views'));
+        (new Filesystem)->copyDirectory($spladeBreezeStubsDir.'resources/views', resource_path('views'));
 
         if (! $this->option('dark')) {
             $this->removeDarkClasses(
@@ -66,31 +66,31 @@ trait InstallsSpladeStack
             );
         }
 
-        copy($spladeBreezeStubsDir . 'resources/views/dashboard.blade.php', resource_path('views/dashboard.blade.php'));
-        copy($spladeBreezeStubsDir . 'resources/views/root.blade.php', resource_path('views/root.blade.php'));
-        copy($spladeBreezeStubsDir . 'resources/views/welcome.blade.php', resource_path('views/welcome.blade.php'));
+        copy($spladeBreezeStubsDir.'resources/views/dashboard.blade.php', resource_path('views/dashboard.blade.php'));
+        copy($spladeBreezeStubsDir.'resources/views/root.blade.php', resource_path('views/root.blade.php'));
+        copy($spladeBreezeStubsDir.'resources/views/welcome.blade.php', resource_path('views/welcome.blade.php'));
 
         // Components...
         (new Filesystem)->ensureDirectoryExists(app_path('View/Components'));
-        (new Filesystem)->copyDirectory($defaultStubsDir . 'App/View/Components', app_path('View/Components'));
+        (new Filesystem)->copyDirectory($defaultStubsDir.'App/View/Components', app_path('View/Components'));
 
         // Tests...
         $this->installTests();
 
         // Routes...
-        copy($spladeBreezeStubsDir . 'routes/web.php', base_path('routes/web.php'));
-        copy($spladeBreezeStubsDir . 'routes/auth.php', base_path('routes/auth.php'));
+        copy($spladeBreezeStubsDir.'routes/web.php', base_path('routes/web.php'));
+        copy($spladeBreezeStubsDir.'routes/auth.php', base_path('routes/auth.php'));
 
         // "Dashboard" Route...
         $this->replaceInFile('/home', '/dashboard', app_path('Providers/RouteServiceProvider.php'));
 
         // Tailwind / Vite...
-        copy($spladeBaseStubsDir . 'tailwind.config.js', base_path('tailwind.config.js'));
-        copy($spladeBaseStubsDir . 'postcss.config.js', base_path('postcss.config.js'));
-        copy($spladeBaseStubsDir . 'vite.config.js', base_path('vite.config.js'));
-        copy($spladeBaseStubsDir . 'resources/css/app.css', resource_path('css/app.css'));
-        copy($spladeBaseStubsDir . 'resources/js/app.js', resource_path('js/app.js'));
-        copy($spladeBaseStubsDir . 'resources/js/ssr.js', resource_path('js/ssr.js'));
+        copy($spladeBaseStubsDir.'tailwind.config.js', base_path('tailwind.config.js'));
+        copy($spladeBaseStubsDir.'postcss.config.js', base_path('postcss.config.js'));
+        copy($spladeBaseStubsDir.'vite.config.js', base_path('vite.config.js'));
+        copy($spladeBaseStubsDir.'resources/css/app.css', resource_path('css/app.css'));
+        copy($spladeBaseStubsDir.'resources/js/app.js', resource_path('js/app.js'));
+        copy($spladeBaseStubsDir.'resources/js/ssr.js', resource_path('js/ssr.js'));
 
         if (file_exists(base_path('pnpm-lock.yaml'))) {
             $this->runCommands(['pnpm install', 'pnpm run build']);
